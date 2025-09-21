265ff583f8cca495b4ee0a39261a61b06fcbfe74
singhakash123 <akashlife2004@gmail.com>
2025-09-21 10:29:50 +0530

    Add comments to multer middleware for better clarity on usage

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -16,3 +16,8 @@ export const upload = multer(
   storage
   }
 )
+
+// fields should be an array of objects with name and optionally a maxCount. Example:
+
+// destination is used to determine within which folder the uploaded files should be stored. This can also be given as a string (e.g. '/tmp/uploads'). If no destination is given, the operating system’s default directory for temporary files is used.
+
