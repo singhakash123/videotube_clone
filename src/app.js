9160cd5db54f5647cae358ada89106a717e4f7f6
singhakash123 <akashlife2004@gmail.com>
2025-09-22 11:54:09 +0530

    Add middleware for security, logging, and rate limiting; update dependencies

--- src/app.js
+++ src/app.js
@@ -1,17 +1,89 @@
-import express from "express" 
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import rateLimit from "express-rate-limit"
+import helmet from "helmet"
+import morgan from "morgan"
+import { limit } from "./constant.js"
 
-export const app = express()
+// App configuration
+export const app = express();
 
 
+// --------------------- MIDDLEWARE SETUP ---------------------
+// 1. Security Middlewares : 
+  app.use(helmet())
+  app.use(cors({
+       credentials : true , 
+       origin : process.env.CORS_ORIGIN 
+  }))
+
+app.use(cookieParser())
+
+// 2. Body Parsing : 
+app.use(express.json({
+     limit
+}))  // from the form 
+
+app.use(express.urlencoded({
+  extended : true , 
+  limit
+}))  // from the url 
 
 
+// 3. Static File Serving : 
+app.use(express.static('public'))  // for the pdf or other : 
 
+// 4 . rate limiting : 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per IP
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
 
-
-
-
+// loging : 
+app.use(morgan("dev"))
 
 // router : 
-// Mount all routes at /api/v1
+// Routes : 
+import { router } from "./routes/index.js"
+// prefix for API
+app.use("/api/v1", router);
+
+/*
+🏆 Final API Endpoints
+POST    /api/v1/user/register → register user
+GET /api/v1/video/ → get all videos
+*/
+
+
+
+/*
+
+
+how to setup routes from here : 
 import { router } from "./routes/index.js";
-app.use('/api/v1', router);
+
+
+ --------------------- ROUTES ---------------------
+app.use("/api/v1", router);
+
+
+
+
+🔥 Final Clean Endpoints:
+
+POST /api/v1/user/register
+POST /api/v1/user/login
+GET /api/v1/book
+POST /api/v1/book/add
+
+               ✅ Final Clean Endpoints
+POST http://localhost:8000/api/v1/user/register
+POST http://localhost:8000/api/v1/user/login
+POST http://localhost:8000/api/v1/book/add
+GET http://localhost:8000/api/v1/book
+
+*/
--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,2 @@
 export const db_name = "youtube_clone" 
+export const limit = "16kb"
